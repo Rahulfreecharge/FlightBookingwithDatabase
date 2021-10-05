53bbb1ea13f89f4cb6ef635a89389b30f55a14cd
Rahulfreecharge <91590439+Rahulfreecharge@users.noreply.github.com>
2021-10-05 12:06:39 +0530

    Add files via upload

---
+++ FlightBookingApp-Assignment-2--master/src/main/java/com/example/flightBooking/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.example.flightBooking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, Integer>{
+	Ticket findByPnr(int pnr);
+	Ticket findByemail(String email);
+	
+}
---
+++ FlightBookingApp-Assignment-2--master/src/main/java/com/example/flightBooking/TicketService.java
@@ -0,0 +1,29 @@
+package com.example.flightBooking;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+	@Autowired
+	TicketRepository ticketRepository;
+	
+	Ticket getTicket(int pnr) {
+		return ticketRepository.findByPnr(pnr);
+	}
+	Ticket getTicket(String email) {
+		return ticketRepository.findByemail(email);
+	}
+	
+	
+	void addTicket(Ticket ticket,int flightId) {
+		Random r = new Random();
+		int pnr = 100000 + (int)(r.nextFloat() * 899900);
+		ticket.setFlightId(flightId);
+		ticket.setPnr(pnr);
+		ticketRepository.save(ticket);
+	}
+	
+}
